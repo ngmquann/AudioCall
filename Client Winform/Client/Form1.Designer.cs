60cfe0100aeb86300bcc128d2fa96855139c01a8
tgh2251120018 <2251120018@ut.edu.vn>
2024-11-03 18:30:08 +0700

    Add files via upload

---
+++ Client Winform/Client/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Client
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private System.Windows.Forms.Button introductionButton;
+        private System.Windows.Forms.Button callButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button historyButton; 
+        private System.Windows.Forms.Panel sidePanel;
+        private System.Windows.Forms.Panel mainPanel;
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.introductionButton = new System.Windows.Forms.Button();
+            this.callButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.historyButton = new System.Windows.Forms.Button(); 
+            this.sidePanel = new System.Windows.Forms.Panel();
+            this.mainPanel = new System.Windows.Forms.Panel();
+
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Audio Call Control - Client";
+            this.BackColor = Color.Black; 
+
+            this.sidePanel.Dock = DockStyle.Left;
+            this.sidePanel.Width = 200;
+            this.sidePanel.BackColor = Color.Gray;
+            this.sidePanel.Controls.Add(this.introductionButton);
+            this.sidePanel.Controls.Add(this.callButton);
+            this.sidePanel.Controls.Add(this.historyButton); 
+            this.sidePanel.Controls.Add(this.exitButton);
+
+            
+            this.introductionButton.Location = new Point(20, 50);
+            this.introductionButton.Name = "introductionButton";
+            this.introductionButton.Size = new Size(160, 50);
+            this.introductionButton.Text = "Introduction";
+            this.introductionButton.UseVisualStyleBackColor = true;
+            this.introductionButton.BackColor = Color.White;
+            this.introductionButton.Font = new Font("Arial", 12, FontStyle.Bold); 
+            this.introductionButton.FlatStyle = FlatStyle.Flat; 
+            this.introductionButton.Click += new System.EventHandler(this.IntroductionButton_Click);
+
+            
+            this.callButton.Location = new Point(20, 120);
+            this.callButton.Name = "callButton";
+            this.callButton.Size = new Size(160, 50);
+            this.callButton.Text = "Call";
+            this.callButton.UseVisualStyleBackColor = true;
+            this.callButton.BackColor = Color.White;
+            this.callButton.Font = new Font("Arial", 12, FontStyle.Bold); 
+            this.callButton.FlatStyle = FlatStyle.Flat; 
+            this.callButton.Click += new System.EventHandler(this.CallButton_Click);
+
+            
+            this.historyButton.Location = new Point(20, 190); 
+            this.historyButton.Name = "historyButton";
+            this.historyButton.Size = new Size(160, 50);
+            this.historyButton.Text = "History";
+            this.historyButton.UseVisualStyleBackColor = true;
+            this.historyButton.BackColor = Color.White;
+            this.historyButton.Font = new Font("Arial", 12, FontStyle.Bold);
+            this.historyButton.FlatStyle = FlatStyle.Flat; 
+            this.historyButton.Click += new System.EventHandler(this.HistoryButton_Click); 
+
+            
+            this.exitButton.Location = new Point(20, 260);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new Size(160, 50);
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.BackColor = Color.White;
+            this.exitButton.Font = new Font("Arial", 12, FontStyle.Bold);
+            this.exitButton.FlatStyle = FlatStyle.Flat; 
+            this.exitButton.Click += new System.EventHandler(this.ExitButton_Click);
+
+            
+            this.mainPanel.Dock = DockStyle.Fill;
+            this.mainPanel.BackColor = Color.Black;
+
+            
+            this.Controls.Add(this.mainPanel);
+            this.Controls.Add(this.sidePanel);
+        }
+
+        #endregion
+    }
+}
---
+++ Client Winform/Client/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic; 
+using System.Drawing; 
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        private List<string> connectionHistory; 
+
+        public Form1()
+        {
+            InitializeComponent();
+            connectionHistory = new List<string>(); 
+            ShowIntroduction();
+        }
+
+        private void ShowIntroduction()
+        {
+            mainPanel.Controls.Clear();
+            Label introLabel = new Label
+            {
+                Text = "Ứng dụng gọi Audio giữa Client và Server",
+                AutoSize = false,
+                TextAlign = ContentAlignment.MiddleCenter,
+                Dock = DockStyle.Fill,
+                Font = new Font("Arial", 16, FontStyle.Bold),
+                ForeColor = Color.White,
+                BackColor = Color.Black
+            };
+            mainPanel.Controls.Add(introLabel);
+        }
+
+        private void ShowCallInterface()
+        {
+            mainPanel.Controls.Clear();
+
+           
+            TextBox ipAddressTextBox = new TextBox
+            {
+                Location = new Point(200, 50),
+                Name = "ipAddressTextBox",
+                Size = new Size(250, 30),
+                Font = new Font("Arial", 12),
+                ForeColor = Color.Gray,
+                Text = "Nhập địa chỉ IP của Server"
+            };
+
+            
+            ipAddressTextBox.GotFocus += (s, e) =>
+            {
+                if (ipAddressTextBox.Text == "Nhập địa chỉ IP của Server")
+                {
+                    ipAddressTextBox.Text = "";
+                    ipAddressTextBox.ForeColor = Color.Black;
+                }
+            };
+
+           
+            ipAddressTextBox.LostFocus += (s, e) =>
+            {
+                if (string.IsNullOrWhiteSpace(ipAddressTextBox.Text))
+                {
+                    ipAddressTextBox.Text = "Nhập địa chỉ IP của Server";
+                    ipAddressTextBox.ForeColor = Color.Gray;
+                }
+            };
+
+
+            Button connectButton = new Button
+            {
+                Location = new Point(200, 100),
+                Name = "connectButton",
+                Size = new Size(100, 50),
+                Text = "Connect",
+                UseVisualStyleBackColor = true,
+                BackColor = Color.LightGreen,
+                Font = new Font("Arial", 12, FontStyle.Bold),
+                FlatStyle = FlatStyle.Flat
+            };
+            connectButton.Anchor = AnchorStyles.Top;
+            connectButton.MouseEnter += (s, e) => connectButton.BackColor = Color.Green;
+            connectButton.MouseLeave += (s, e) => connectButton.BackColor = Color.LightGreen;
+            connectButton.Click += new EventHandler(this.StartButton_Click);
+
+            Button endButton = new Button
+            {
+                Location = new Point(350, 100),
+                Name = "endButton",
+                Size = new Size(100, 50),
+                Text = "End Call",
+                UseVisualStyleBackColor = true,
+                BackColor = Color.IndianRed,
+                Font = new Font("Arial", 12, FontStyle.Bold),
+                FlatStyle = FlatStyle.Flat
+            };
+            endButton.Anchor = AnchorStyles.Top;
+            endButton.MouseEnter += (s, e) => endButton.BackColor = Color.Red;
+            endButton.MouseLeave += (s, e) => endButton.BackColor = Color.IndianRed;
+            endButton.Click += new EventHandler(this.EndButton_Click);
+
+            Label myLabel = new Label
+            {
+                Location = new Point(200, 180),
+                Name = "myLabel",
+                Size = new Size(250, 40),
+                Text = "Status: Idle",
+                Font = new Font("Arial", 14, FontStyle.Bold),
+                TextAlign = ContentAlignment.MiddleCenter,
+                ForeColor = Color.White
+            };
+            myLabel.Anchor = AnchorStyles.Top;
+
+            TrackBar volumeBar = new TrackBar
+            {
+                Location = new Point(200, 240),
+                Name = "volumeBar",
+                Size = new Size(250, 45),
+                Minimum = 0,
+                Maximum = 100,
+                Value = 50,
+                TickFrequency = 10,
+                BackColor = Color.Black
+            };
+            volumeBar.Anchor = AnchorStyles.Top;
+
+            mainPanel.Controls.Add(ipAddressTextBox);
+            mainPanel.Controls.Add(connectButton);
+            mainPanel.Controls.Add(endButton);
+            mainPanel.Controls.Add(myLabel);
+            mainPanel.Controls.Add(volumeBar);
+        }
+
+        private void ShowHistory()
+        {
+            mainPanel.Controls.Clear();
+            ListBox historyListBox = new ListBox
+            {
+                Location = new Point(200, 50),
+                Name = "historyListBox",
+                Size = new Size(400, 300),
+                Font = new Font("Arial", 12)
+            };
+
+           
+            foreach (var ip in connectionHistory)
+            {
+                historyListBox.Items.Add(ip);
+            }
+
+            Button backButton = new Button
+            {
+                Location = new Point(200, 370),
+                Name = "backButton",
+                Size = new Size(100, 50),
+                Text = "Back",
+                UseVisualStyleBackColor = true,
+                BackColor = Color.LightGray,
+                Font = new Font("Arial", 12, FontStyle.Bold),
+                FlatStyle = FlatStyle.Flat
+            };
+            backButton.Click += (s, e) => ShowCallInterface(); 
+
+            mainPanel.Controls.Add(historyListBox);
+            mainPanel.Controls.Add(backButton);
+        }
+
+        private void IntroductionButton_Click(object sender, EventArgs e)
+        {
+            ShowIntroduction();
+        }
+
+        private void CallButton_Click(object sender, EventArgs e)
+        {
+            ShowCallInterface();
+        }
+
+        private void HistoryButton_Click(object sender, EventArgs e)
+        {
+            ShowHistory();
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            // Retrieve IP address from the text box
+            string ipAddress = string.Empty;
+            foreach (Control control in mainPanel.Controls)
+            {
+                if (control is TextBox textBox && textBox.Name == "ipAddressTextBox")
+                {
+                    ipAddress = textBox.Text;
+                }
+                if (control is Label label && label.Name == "myLabel")
+                {
+                    label.Text = "Status: Connecting...";
+                    label.ForeColor = Color.Green;
+                }
+            }
+
+           
+            if (!string.IsNullOrWhiteSpace(ipAddress) && !connectionHistory.Contains(ipAddress))
+            {
+                connectionHistory.Add(ipAddress);
+            }
+        }
+
+        private void EndButton_Click(object sender, EventArgs e)
+        {
+            foreach (Control control in mainPanel.Controls)
+            {
+                if (control is Label label && label.Name == "myLabel")
+                {
+                    label.Text = "Status: Call Ended";
+                    label.ForeColor = Color.Red;
+                }
+            }
+        }
+    }
+}
