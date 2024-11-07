485707c00fa1a88a036e94e620c707be8d35591e
ngmquann <117283361+ngmquann@users.noreply.github.com>
2024-11-07 11:46:49 +0700

    update client

--- Client Winform/Client/Form1.Designer.cs
+++ Client Winform/Client/Form1.Designer.cs
@@ -1,4 +1,5 @@
 ﻿using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
 using System.Windows.Forms;
 
 namespace Client
@@ -25,95 +26,130 @@ namespace Client
 
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             this.introductionButton = new System.Windows.Forms.Button();
             this.callButton = new System.Windows.Forms.Button();
             this.exitButton = new System.Windows.Forms.Button();
             this.historyButton = new System.Windows.Forms.Button();
             this.sidePanel = new System.Windows.Forms.Panel();
             this.mainPanel = new System.Windows.Forms.Panel();
-
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Audio Call Control - Client";
-            this.BackColor = Color.FromArgb(242, 242, 242);
-
-            this.sidePanel.Dock = DockStyle.Left;
-            this.sidePanel.Width = 200;
-            this.sidePanel.BackColor = Color.FromArgb(153, 102, 204);
+            this.sidePanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // introductionButton
+            // 
+            this.introductionButton.BackColor = System.Drawing.Color.White;
+            this.introductionButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.introductionButton.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.introductionButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.introductionButton.Location = new System.Drawing.Point(20, 50);
+            this.introductionButton.Name = "introductionButton";
+            this.introductionButton.Size = new System.Drawing.Size(160, 50);
+            this.introductionButton.TabIndex = 0;
+            this.introductionButton.Text = "Introduction";
+            this.introductionButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.introductionButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.introductionButton.UseVisualStyleBackColor = false;
+            this.introductionButton.Click += new System.EventHandler(this.IntroductionButton_Click);
+            // 
+            // callButton
+            // 
+            this.callButton.BackColor = System.Drawing.Color.White;
+            this.callButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.callButton.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.callButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.callButton.Location = new System.Drawing.Point(20, 120);
+            this.callButton.Name = "callButton";
+            this.callButton.Size = new System.Drawing.Size(160, 50);
+            this.callButton.TabIndex = 1;
+            this.callButton.Text = "Call";
+            this.callButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.callButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.callButton.UseVisualStyleBackColor = false;
+            this.callButton.Click += new System.EventHandler(this.CallButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.BackColor = System.Drawing.Color.White;
+            this.exitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exitButton.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.exitButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.exitButton.Location = new System.Drawing.Point(20, 260);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(160, 50);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "Exit";
+            this.exitButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.exitButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.exitButton.UseVisualStyleBackColor = false;
+            this.exitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // historyButton
+            // 
+            this.historyButton.BackColor = System.Drawing.Color.White;
+            this.historyButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.historyButton.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.historyButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.historyButton.Location = new System.Drawing.Point(20, 190);
+            this.historyButton.Name = "historyButton";
+            this.historyButton.Size = new System.Drawing.Size(160, 50);
+            this.historyButton.TabIndex = 2;
+            this.historyButton.Text = "History";
+            this.historyButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.historyButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.historyButton.UseVisualStyleBackColor = false;
+            this.historyButton.Click += new System.EventHandler(this.HistoryButton_Click);
+            // 
+            // sidePanel
+            // 
+            this.sidePanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(102)))), ((int)(((byte)(204)))));
             this.sidePanel.Controls.Add(this.introductionButton);
             this.sidePanel.Controls.Add(this.callButton);
             this.sidePanel.Controls.Add(this.historyButton);
             this.sidePanel.Controls.Add(this.exitButton);
-
-            Bitmap ResizeIcon(string path, int width, int height)
-            {
-                Bitmap originalIcon = new Bitmap(path);
-                return new Bitmap(originalIcon, new Size(width, height));
-            }
-
-            this.introductionButton.Image = ResizeIcon("icons/intro.png", 24, 24);
-            this.introductionButton.ImageAlign = ContentAlignment.MiddleLeft;
-            this.introductionButton.TextImageRelation = TextImageRelation.ImageBeforeText;
-            this.introductionButton.Location = new Point(20, 50);
-            this.introductionButton.Name = "introductionButton";
-            this.introductionButton.Size = new Size(160, 50);
-            this.introductionButton.Text = "Introduction";
-            this.introductionButton.TextAlign = ContentAlignment.MiddleRight;
-            this.introductionButton.BackColor = Color.White;
-            this.introductionButton.Font = new Font("Arial", 12, FontStyle.Bold);
-            this.introductionButton.FlatStyle = FlatStyle.Flat;
-            this.introductionButton.Click += new System.EventHandler(this.IntroductionButton_Click);
-
-            this.callButton.Image = ResizeIcon("icons/call.png", 24, 24);
-            this.callButton.ImageAlign = ContentAlignment.MiddleLeft;
-            this.callButton.TextImageRelation = TextImageRelation.ImageBeforeText;
-            this.callButton.Location = new Point(20, 120);
-            this.callButton.Name = "callButton";
-            this.callButton.Size = new Size(160, 50);
-            this.callButton.Text = "Call";
-            this.callButton.TextAlign = ContentAlignment.MiddleRight;
-            this.callButton.UseVisualStyleBackColor = true;
-            this.callButton.BackColor = Color.White;
-            this.callButton.Font = new Font("Arial", 12, FontStyle.Bold);
-            this.callButton.FlatStyle = FlatStyle.Flat;
-            this.callButton.Click += new System.EventHandler(this.CallButton_Click);
-
-            this.historyButton.Image = ResizeIcon("icons/history.png", 24, 24);
-            this.historyButton.ImageAlign = ContentAlignment.MiddleLeft;
-            this.historyButton.TextImageRelation = TextImageRelation.ImageBeforeText;
-            this.historyButton.Location = new Point(20, 190);
-            this.historyButton.Name = "historyButton";
-            this.historyButton.Size = new Size(160, 50);
-            this.historyButton.Text = "History";
-            this.historyButton.TextAlign = ContentAlignment.MiddleRight;
-            this.historyButton.UseVisualStyleBackColor = true;
-            this.historyButton.BackColor = Color.White;
-            this.historyButton.Font = new Font("Arial", 12, FontStyle.Bold);
-            this.historyButton.FlatStyle = FlatStyle.Flat;
-            this.historyButton.Click += new System.EventHandler(this.HistoryButton_Click);
-
-            this.exitButton.Image = ResizeIcon("icons/exit.png", 24, 24);
-            this.exitButton.ImageAlign = ContentAlignment.MiddleLeft;
-            this.exitButton.TextImageRelation = TextImageRelation.ImageBeforeText;
-            this.exitButton.Location = new Point(20, 260);
-            this.exitButton.Name = "exitButton";
-            this.exitButton.Size = new Size(160, 50);
-            this.exitButton.Text = "Exit";
-            this.exitButton.TextAlign = ContentAlignment.MiddleRight;
-            this.exitButton.UseVisualStyleBackColor = true;
-            this.exitButton.BackColor = Color.White;
-            this.exitButton.Font = new Font("Arial", 12, FontStyle.Bold);
-            this.exitButton.FlatStyle = FlatStyle.Flat;
-            this.exitButton.Click += new System.EventHandler(this.ExitButton_Click);
-
-            this.mainPanel.Dock = DockStyle.Fill;
-            this.mainPanel.BackColor = Color.FromArgb(47, 54, 64);
-
+            this.sidePanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.sidePanel.Location = new System.Drawing.Point(0, 0);
+            this.sidePanel.Name = "sidePanel";
+            this.sidePanel.Size = new System.Drawing.Size(200, 450);
+            this.sidePanel.TabIndex = 1;
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(54)))), ((int)(((byte)(64)))));
+            this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPanel.Location = new System.Drawing.Point(200, 0);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(600, 450);
+            this.mainPanel.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.mainPanel);
             this.Controls.Add(this.sidePanel);
-
+            this.Name = "Form1";
+            this.Text = "Audio Call Control - Client";
+            this.sidePanel.ResumeLayout(false);
             this.ResumeLayout(false);
+
+        }
+
+        private static Bitmap ResizeIcon(string path, int width, int height)
+        {
+            using (Bitmap originalIcon = new Bitmap(path))
+            {
+                return new Bitmap(originalIcon, new Size(width, height));
+            }
+        }
+
+        private void SetButtonIcons()
+        {
+            this.introductionButton.Image = ResizeIcon("icons/intro.png", 24, 24);
+            this.callButton.Image = ResizeIcon("icons/call.png", 24, 24);
+            this.historyButton.Image = ResizeIcon("icons/history.png", 24, 24);
+            this.exitButton.Image = ResizeIcon("icons/exit.png", 24, 24);
         }
     }
 }
--- Client Winform/Client/Form1.cs
+++ Client Winform/Client/Form1.cs
@@ -1,19 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Net.Sockets;
+using System.Threading.Tasks;
 using System.Windows.Forms;
+using NAudio.Wave;
 
 namespace Client
 {
     public partial class Form1 : Form
     {
+        private WaveIn waveIn;
+        private WaveOut waveOut;
+        private BufferedWaveProvider waveProvider;
+        private TcpClient client;
+        private TcpListener server;
+        private NetworkStream stream;
+        private bool isServer;
+        private bool isConnected = false;
         private List<string> connectionHistory;
 
         public Form1()
         {
             InitializeComponent();
             connectionHistory = new List<string>();
+            InitializeAudio();
             ShowIntroduction();
+        }
+
+        private void InitializeAudio()
+        {
+            waveIn = new WaveIn();
+            waveIn.WaveFormat = new WaveFormat(44100, 1);
+            waveIn.DataAvailable += WaveIn_DataAvailable;
+            waveIn.BufferMilliseconds = 50;
         }
 
         private void ShowIntroduction()
@@ -27,7 +47,7 @@ namespace Client
                 Dock = DockStyle.Fill,
                 Font = new Font("Arial", 16, FontStyle.Bold),
                 ForeColor = Color.White,
-                BackColor = Color.FromArgb(47, 54, 64) 
+                BackColor = Color.FromArgb(47, 54, 64)
             };
             mainPanel.Controls.Add(introLabel);
         }
@@ -78,7 +98,7 @@ namespace Client
             connectButton.Anchor = AnchorStyles.Top;
             connectButton.MouseEnter += (s, e) => connectButton.BackColor = Color.Green;
             connectButton.MouseLeave += (s, e) => connectButton.BackColor = Color.LightGreen;
-            connectButton.Click += new EventHandler(this.StartButton_Click);
+            connectButton.Click += new EventHandler(this.ConnectButton_Click);
 
             Button endButton = new Button
             {
@@ -89,24 +109,25 @@ namespace Client
                 UseVisualStyleBackColor = true,
                 BackColor = Color.IndianRed,
                 Font = new Font("Arial", 12, FontStyle.Bold),
-                FlatStyle = FlatStyle.Flat
+                FlatStyle = FlatStyle.Flat,
+                Enabled = false
             };
             endButton.Anchor = AnchorStyles.Top;
             endButton.MouseEnter += (s, e) => endButton.BackColor = Color.Red;
             endButton.MouseLeave += (s, e) => endButton.BackColor = Color.IndianRed;
             endButton.Click += new EventHandler(this.EndButton_Click);
 
-            Label myLabel = new Label
+            Label statusLabel = new Label
             {
                 Location = new Point(100, 180),
-                Name = "myLabel",
+                Name = "statusLabel",
                 Size = new Size(250, 40),
                 Text = "Status: Idle",
                 Font = new Font("Arial", 14, FontStyle.Bold),
                 TextAlign = ContentAlignment.MiddleCenter,
                 ForeColor = Color.White
             };
-            myLabel.Anchor = AnchorStyles.Top;
+            statusLabel.Anchor = AnchorStyles.Top;
 
             TrackBar volumeBar = new TrackBar
             {
@@ -117,7 +138,7 @@ namespace Client
                 Maximum = 100,
                 Value = 50,
                 TickFrequency = 10,
-                BackColor = Color.FromArgb(47, 54, 64) 
+                BackColor = Color.FromArgb(47, 54, 64)
             };
             volumeBar.Anchor = AnchorStyles.Top;
 
@@ -131,16 +152,23 @@ namespace Client
                 ForeColor = Color.White,
                 TextAlign = ContentAlignment.MiddleLeft
             };
-            volumeBar.Scroll += (s, e) => volumeLabel.Text = volumeBar.Value + "%";
+
+            volumeBar.ValueChanged += (s, e) =>
+            {
+                volumeLabel.Text = volumeBar.Value + "%";
+                if (waveOut != null)
+                {
+                    waveOut.Volume = volumeBar.Value / 100f;
+                }
+            };
 
             mainPanel.Controls.Add(ipAddressTextBox);
             mainPanel.Controls.Add(connectButton);
             mainPanel.Controls.Add(endButton);
-            mainPanel.Controls.Add(myLabel);
+            mainPanel.Controls.Add(statusLabel);
             mainPanel.Controls.Add(volumeBar);
             mainPanel.Controls.Add(volumeLabel);
         }
-
 
         private void ShowHistory()
         {
@@ -151,7 +179,8 @@ namespace Client
                 Name = "historyListBox",
                 Size = new Size(400, 300),
                 Font = new Font("Arial", 12),
-                BackColor = Color.Black 
+                BackColor = Color.FromArgb(47, 54, 64),
+                ForeColor = Color.White
             };
 
             foreach (var ip in connectionHistory)
@@ -176,6 +205,201 @@ namespace Client
             mainPanel.Controls.Add(backButton);
         }
 
+        private async void ConnectButton_Click(object sender, EventArgs e)
+        {
+            if (!isConnected)
+            {
+                var connectButton = mainPanel.Controls.Find("connectButton", false)[0] as Button;
+                var statusLabel = mainPanel.Controls.Find("statusLabel", false)[0] as Label;
+                var ipAddressTextBox = mainPanel.Controls.Find("ipAddressTextBox", false)[0] as TextBox;
+                var endButton = mainPanel.Controls.Find("endButton", false)[0] as Button;
+
+                connectButton.Enabled = false;
+                statusLabel.Text = "Status: Connecting...";
+                statusLabel.ForeColor = Color.Yellow;
+
+                try
+                {
+                    await StartClientAsync(ipAddressTextBox.Text);
+                    if (!string.IsNullOrWhiteSpace(ipAddressTextBox.Text) &&
+                        !connectionHistory.Contains(ipAddressTextBox.Text))
+                    {
+                        connectionHistory.Add(ipAddressTextBox.Text);
+                    }
+                    waveIn.StartRecording();
+                    endButton.Enabled = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Connection error: {ex.Message}", "Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    statusLabel.Text = "Status: Connection failed";
+                    statusLabel.ForeColor = Color.Red;
+                    connectButton.Enabled = true;
+                    return;
+                }
+            }
+        }
+
+        private async Task StartClientAsync(string serverIP)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(serverIP))
+                {
+                    throw new Exception("Please enter server IP address");
+                }
+
+                client = new TcpClient();
+                await client.ConnectAsync(serverIP, 8000);
+                stream = client.GetStream();
+
+                SetupSuccessfulConnection();
+                var statusLabel = mainPanel.Controls.Find("statusLabel", false)[0] as Label;
+                statusLabel.Text = "Status: Connected";
+                statusLabel.ForeColor = Color.Green;
+            }
+            catch (Exception)
+            {
+                if (client != null) client.Close();
+                throw;
+            }
+        }
+
+        private void SetupSuccessfulConnection()
+        {
+            isConnected = true;
+            var ipAddressTextBox = mainPanel.Controls.Find("ipAddressTextBox", false)[0] as TextBox;
+            ipAddressTextBox.Enabled = false;
+
+            InitializeAudioDevices();
+            StartReceivingAudio();
+        }
+
+        private void InitializeAudioDevices()
+        {
+            try
+            {
+                waveOut = new WaveOut();
+                waveProvider = new BufferedWaveProvider(new WaveFormat(44100, 1));
+                waveOut.Init(waveProvider);
+                waveOut.Play();
+
+                var volumeBar = mainPanel.Controls.Find("volumeBar", false)[0] as TrackBar;
+                waveOut.Volume = volumeBar.Value / 100f;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error initializing audio devices: {ex.Message}",
+                    "Audio Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void WaveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            try
+            {
+                if (stream != null && stream.CanWrite)
+                {
+                    stream.Write(e.Buffer, 0, e.BytesRecorded);
+                }
+            }
+            catch (Exception ex)
+            {
+                this.Invoke((MethodInvoker)delegate
+                {
+                    var statusLabel = mainPanel.Controls.Find("statusLabel", false)[0] as Label;
+                    statusLabel.Text = $"Status: Audio sending error";
+                    statusLabel.ForeColor = Color.Red;
+                });
+            }
+        }
+
+        private void StartReceivingAudio()
+        {
+            Task.Run(async () =>
+            {
+                byte[] buffer = new byte[4096];
+                while (isConnected && stream != null)
+                {
+                    try
+                    {
+                        int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+                        if (bytesRead > 0)
+                        {
+                            waveProvider.AddSamples(buffer, 0, bytesRead);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        if (isConnected)
+                        {
+                            this.Invoke((MethodInvoker)delegate
+                            {
+                                var statusLabel = mainPanel.Controls.Find("statusLabel", false)[0] as Label;
+                                statusLabel.Text = "Status: Audio receiving error";
+                                statusLabel.ForeColor = Color.Red;
+                            });
+                        }
+                        break;
+                    }
+                }
+            });
+        }
+
+        private void EndButton_Click(object sender, EventArgs e)
+        {
+            EndCall();
+        }
+
+        private void EndCall()
+        {
+            isConnected = false;
+
+            if (waveIn != null)
+            {
+                waveIn.StopRecording();
+                waveIn.Dispose();
+                waveIn = null;
+            }
+
+            if (waveOut != null)
+            {
+                waveOut.Stop();
+                waveOut.Dispose();
+                waveOut = null;
+            }
+
+            if (stream != null)
+            {
+                stream.Close();
+                stream = null;
+            }
+
+            if (client != null)
+            {
+                client.Close();
+                client = null;
+            }
+
+            if (server != null)
+            {
+                server.Stop();
+                server = null;
+            }
+
+            var connectButton = mainPanel.Controls.Find("connectButton", false)[0] as Button;
+            var endButton = mainPanel.Controls.Find("endButton", false)[0] as Button;
+            var ipAddressTextBox = mainPanel.Controls.Find("ipAddressTextBox", false)[0] as TextBox;
+            var statusLabel = mainPanel.Controls.Find("statusLabel", false)[0] as Label;
+
+            connectButton.Enabled = true;
+            endButton.Enabled = false;
+            ipAddressTextBox.Enabled = true;
+            statusLabel.Text = "Status: Disconnected";
+            statusLabel.ForeColor = Color.White;
+        }
+
         private void IntroductionButton_Click(object sender, EventArgs e)
         {
             ShowIntroduction();
@@ -194,40 +418,6 @@ namespace Client
         private void ExitButton_Click(object sender, EventArgs e)
         {
             Application.Exit();
-        }
-
-        private void StartButton_Click(object sender, EventArgs e)
-        {
-            string ipAddress = string.Empty;
-            foreach (Control control in mainPanel.Controls)
-            {
-                if (control is TextBox textBox && textBox.Name == "ipAddressTextBox")
-                {
-                    ipAddress = textBox.Text;
-                }
-                if (control is Label label && label.Name == "myLabel")
-                {
-                    label.Text = "Status: Connecting...";
-                    label.ForeColor = Color.Green;
-                }
-            }
-
-            if (!string.IsNullOrWhiteSpace(ipAddress) && !connectionHistory.Contains(ipAddress))
-            {
-                connectionHistory.Add(ipAddress);
-            }
-        }
-
-        private void EndButton_Click(object sender, EventArgs e)
-        {
-            foreach (Control control in mainPanel.Controls)
-            {
-                if (control is Label label && label.Name == "myLabel")
-                {
-                    label.Text = "Status: Call Ended";
-                    label.ForeColor = Color.Red;
-                }
-            }
         }
     }
 }
